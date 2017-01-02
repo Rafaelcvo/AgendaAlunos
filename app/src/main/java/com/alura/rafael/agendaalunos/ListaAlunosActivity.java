548ecb8ce0f06442ba6590a0947f499f6676a292
Rafaelcvo <rafaelmoreiracvo@yahoo.com.br>
2017-01-02 10:13:54 -0200

    Inserindo o botao camera.

--- app/src/main/java/com/alura/rafael/agendaalunos/ListaAlunosActivity.java
+++ app/src/main/java/com/alura/rafael/agendaalunos/ListaAlunosActivity.java
@@ -4,20 +4,16 @@ import android.Manifest;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.provider.Browser;
 import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.alura.rafael.agendaalunos.dao.AlunoDao;
 import com.alura.rafael.agendaalunos.modelo.Aluno;
@@ -31,7 +27,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_lista_alunos);
 
         /*String[] alunos = {"Rafael", "Antonio", "Marcelo", "Alberto", "Rafael", "Antonio",
                 "Marcelo", "Alberto", "Rafael", "Antonio", "Marcelo", "Alberto"};*/
